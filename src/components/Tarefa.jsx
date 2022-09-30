a432f69523cdf747c9590ab3fe75d90be5134b2c
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2022-09-30 16:32:53 -0300

    :sparkles: Create component Tarefa.jsx

---
+++ src/components/Tarefa.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DivTarefa } from "../style/StyledTarefas";
+
+export default function Tarefa() {
+  return (
+    <DivTarefa>
+      <h2>Relatório de Vendas</h2>
+      <p>Para: Departamento de vendas</p>
+      <p>Levantar os valores de vendas deste mês.</p>
+    </DivTarefa>
+  );
+}
