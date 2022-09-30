17215ac37a664c8f907cc839338410fc6754b408
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2022-09-30 16:33:23 -0300

    :lipstick: Using styled-components

---
+++ src/style/StyledTarefas.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+
+//CRIANDO MÓDULO STYLED
+export const DivLista = styled.div`
+    width: 100%;
+    padding: 20px;
+    min-height: 85vh;
+    background-color: #ccc;
+    display: flex; flex-wrap: wrap;
+    justify-content: space-around;
+`
+
+export const DivTarefa = styled.div`
+    background-color: #ffb;
+    border: 2px solid #333;
+    box-shadow: 5px 5px 5px #333;
+    padding: 20px;
+    text-align: center;
+    width: 350px;
+    height: 200px;
+    margin: 10px;
+    h2,p{ padding-bottom: 10px; }
+`
